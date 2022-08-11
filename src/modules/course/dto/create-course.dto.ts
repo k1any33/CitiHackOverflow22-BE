237fbe42959305b04be8a6aa9272368a1f890fc6
Kian Yee <89409516+k1any33@users.noreply.github.com>
2022-08-11 17:33:11 +0800

    feat: add validation for create course dto (#10)

--- src/modules/course/dto/create-course.dto.ts
+++ src/modules/course/dto/create-course.dto.ts
@@ -1,15 +1,22 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 
 export class CreateCourseDto {
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ type: String, example: 'Wealth management course' })
   courseTitle: string
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({
     type: String,
     example: 'This is a financial course from citibank',
   })
   courseDescription: string
 
+  @IsNotEmpty()
+  @IsNumber()
   @ApiProperty({ type: Number, example: 1 })
   courseTier: number
 }
